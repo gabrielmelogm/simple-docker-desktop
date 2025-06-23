c6d670d75f1a7c5d0d39b6c8dde04604e8586173
Gabriel Melo <melogoncalvesbiel@gmail.com>
2025-06-23 13:05:48 -0300

    feat: add Navbar component and integrate navigation menu
    - Introduced a new `Navbar` component to enhance the application's header with navigation links.
    - Implemented a responsive navigation menu using Radix UI components for better user experience.
    - Added supporting `Popover` and `NavigationMenu` components for improved navigation functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { DockerContainers } from "./screens/DockerContainers";
 import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="min-h-screen">
       <main>
+        <Navbar />
         <DockerContainers />
       </main>
     </div>
